019b02b2b7247d4ca5051032b20dd6a1a713419b
javier <ja7373@gmail.com>
2020-08-26 12:16:15 +0200

    primer commit

---
+++ server/sockets/socket.js
@@ -0,0 +1,67 @@
+const { io } = require('../server');
+const { Usuarios } = require('../classes/usuarios');
+const { crearMensaje } = require('../utilidades/utilidades');
+
+const usuarios = new Usuarios();
+
+io.on('connection', (client) => {
+
+    client.on('entrarChat', (data, callback) => {
+        //console.log('entrarChat:', data);
+
+        if (!data.nombre || !data.sala) {
+            return callback({
+                error: true,
+                mensaje: 'El nombre y la sala son necesarios'
+            });
+        }
+
+        client.join(data.sala);
+
+        let personas = usuarios.agregarPersona(client.id, data.nombre, data.sala);
+        // console.log('personas:', personas);
+        client.broadcast.to(data.sala).emit('listaPersona', usuarios.getPersonasPorSala(data.sala));
+
+        callback(usuarios.getPersonasPorSala(data.sala));
+
+        //console.log('Usuario conectado:', data);
+
+    });
+
+    client.on('crearMensaje', (data) => {
+
+        let persona = usuarios.getPersona(client.id); // Usuarios.getPersona(client.id);
+        console.log('Persona desconectada: ', persona);
+        //console.log('crearMensaje-data:', data);
+        let mensaje = crearMensaje(persona.nombre, data.mensaje);
+        //console.log('Socket CrearMensaje:', data);
+        client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+    });
+
+    client.on('disconnect', () => {
+        // console.log('DatosClienteDesconectado:', client);
+        // console.log('Desconectado:', client.id);
+        let personaBorrada = usuarios.borrarPersona(client.id);
+        // console.log('PersonaBorrada:', personaBorrada);
+        //const mesg = { usuario: 'Administrador', mensaje: `${personaBorrada} Ha abandonado el chat` };
+        // const mesg = { usuario: 'Administrador', mensaje: personaBorrada.nombre + ' Ha abandonado el chat' };
+        // console.log('msg:', mesg);
+        //client.broadcast.emit('crearMensaje', { usuario: 'Administrador', mensaje: `${personaBorrada.nombre} Ha abandonado el chat` });
+        client.broadcast.to(personaBorrada.sala).emit('crearMensaje', crearMensaje('Administrador', `${personaBorrada.nombre} Ha abandonado el chat`));
+        client.broadcast.to(personaBorrada.sala).emit('listaPersona', usuarios.getPersonasPorSala(personaBorrada.sala));
+        //console.log('Se ha desconectado', personaBorrada);
+    });
+
+    // mensajes privados
+    client.on('mensajePrivado', (data) => {
+
+        let persona = usuarios.getPersona(client.id);
+        console.log('mensajePrivado: data', data);
+        console.log('mensajePrivado.persona:', persona);
+        if (!persona) return;
+        client.broadcast.to(data.para).emit('mensajePrivado', crearMensaje(persona.nombre, data.mensaje));
+
+    });
+
+
+});
